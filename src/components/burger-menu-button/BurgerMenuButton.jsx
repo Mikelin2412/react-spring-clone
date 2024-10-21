fbe90cededbb554b9d5bedcc1804073163d6ebba
Mikelin2412 <mr_davidov2412@mail.ru>
2024-10-21 17:02:52 +0300

    feat: add burger menu button layout

---
+++ src/components/burger-menu-button/BurgerMenuButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './burgerMenuButton.module.css';
+
+const BurgerMenuButton = () => {
+  return (
+    <div className={styles.burgerMenuButton}>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default BurgerMenuButton;
