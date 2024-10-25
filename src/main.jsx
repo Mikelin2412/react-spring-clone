c8d8ca4df437b1d3e96e7ddd24d6135269a18ce7
Mikelin2412 <mr_davidov2412@mail.ru>
2024-10-25 14:29:33 +0300

    feat: configure routing of application

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import HomePage from './pages/HomePage.jsx';
 import { Provider } from 'react-redux';
 import configureStore from './store/store.js';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes/routerConfig.jsx';
+import './index.css';
 
 const store = configureStore();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <HomePage />
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>,
 );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import Header from '../components/header/Header';
 import MainContent from '../components/main-content/MainContent';
 
 const HomePage = () => {
-  return (
-    <>
-      <Header />
-      <MainContent />
-    </>
-  );
+  return <MainContent />;
 };
 
 export default HomePage;
