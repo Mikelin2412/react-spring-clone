32edb426b5191370d380ae26c4c9e3bf49381c6c
Mikelin2412 <mr_davidov2412@mail.ru>
2024-10-25 17:30:14 +0300

    feat: add simple authorization

--- src/components/login-form/LoginForm.jsx
+++ src/components/login-form/LoginForm.jsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './loginForm.module.css';
+import { useDispatch } from 'react-redux';
+import { authorizeUser, unauthorizeUser } from '../../store/auth/authActions';
+import { useNavigate } from 'react-router-dom';
+import { HOME_ROUTE } from '../../routes/routes';
+import { KEYS } from '../../constants/constants';
 
 const Form = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [hasAccess, setHasAccess] = useState(true);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (username === KEYS.username && password === KEYS.password) {
+      dispatch(authorizeUser(true));
+      navigate(HOME_ROUTE);
+    } else {
+      setHasAccess(false);
+      dispatch(unauthorizeUser(false));
+    }
+  };
+
   return (
     <form className={styles.form}>
       <h3 className={styles.title}>Authorization</h3>
-      <label className={styles.labelText}>Login:</label>
-      <input className={styles.input} type="text" placeholder="Your login" />
+      <label className={styles.labelText}>Username:</label>
+      <input
+        className={styles.input}
+        type="text"
+        placeholder="Your username"
+        onChange={(e) => setUsername(e.target.value)}
+      />
       <label className={styles.labelText}>Password:</label>
       <input
         className={styles.input}
         type="password"
         placeholder="Your password"
+        onChange={(e) => setPassword(e.target.value)}
       />
-      <button className={styles.button} type="submit">Login</button>
+      <button className={styles.button} type="submit" onClick={handleClick}>
+        Login
+      </button>
+      {!hasAccess ? (
+        <span className={styles.warningMessage}>You don't have access!</span>
+      ) : null}
     </form>
   );
 };
--- src/store/auth/authReducers.js
+++ src/store/auth/authReducers.js
@@ -1,26 +1,13 @@
-const INITIAL_STATE = {
-  authData: {
-    username: null,
-    password: null,
-  },
-};
+const INITIAL_STATE = false;
 
 const authReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case 'AUTHORIZE_USER':
-      return {
-        ...state,
-        authData: {
-          ...state.authData,
-          authData: action.payload,
-        },
-      };
+      return action.payload;
 
     case 'UNAUTHORIZE_USER':
-      return {
-        ...state,
-        authData: state.authData.filter((data) => data.id !== action.payload),
-      };
+      return action.payload;
+    
     default:
       return state;
   }
