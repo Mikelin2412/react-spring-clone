0488536bbafd1f944656a26377ca921163814110
Mikelin2412 <mr_davidov2412@mail.ru>
2024-10-26 18:16:09 +0300

    refactor: remove unnecessary comments

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -30,27 +30,3 @@ const AppRouter = () => {
 };
 
 export default AppRouter;
-
-// import { createBrowserRouter } from 'react-router-dom';
-// import HomePage from '../pages/HomePage.jsx';
-// import AuthPage from '../pages/AuthPage.jsx';
-// import { HOME_ROUTE, LOGIN_ROUTE } from './routes.js';
-// import Layout from '../layout.jsx';
-
-// export const router = createBrowserRouter([
-//   {
-//     path: '',
-//     element: <Layout />,
-
-//     children: [
-//       {
-//         path: HOME_ROUTE,
-//         element: <HomePage />,
-//       },
-//       {
-//         path: LOGIN_ROUTE,
-//         element: <AuthPage />,
-//       },
-//     ],
-//   },
-// ]);
